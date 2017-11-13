6b5c66ebc8c0d5201c1de9857e1628c501b04e32
jinghan chen <chenjinghan720@gmail.com>
2017-11-12 21:39:18 -0500

    change

--- pca_getpc.m
+++ pca_getpc.m
@@ -6,23 +6,20 @@ function res = pca_getpc(data)
 [m, n] = size(data);
 MEAN = repmat(mean(data),m,1);
 Xc = data - MEAN;
-[U,S,V1] = svds(Xc);
+[U,S,V1] = fsvd_test(Xc,1480, 0, 0, 0);
 SCORE = V1'*(Xc)';
-x_nrom = sumsqr(data-MEAN);
-res = zeros(n,1);
-for k = 1:n
-    x_hat_c = SCORE(:,1:k)*V1(:,1:k)';
-    x_hat_norm = sumsqr(x_hat_c);
-    frac = x_hat_norm/x_nrom;
-    res(k) = frac;
-end
+res = SCORE';
 
+% set you numpc here, you should acheive 90% reconstruction accuracy
+% numpc = ?;
+
+end
 
 
 
 % set you numpc here, you should acheive 90% reconstruction accuracy
 % numpc = ?;
 
-end
+
 
 
